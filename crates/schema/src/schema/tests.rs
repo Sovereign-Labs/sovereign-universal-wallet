f4b66665f90b0ca5637465090f228cbc4353a7ce
Theodore Bugnet <theodore@sovlabs.io>
2025-05-07 19:29:51 +0100

    Fix scoping and doctests

--- crates/schema/src/schema/tests.rs
+++ crates/schema/src/schema/tests.rs
@@ -15,9 +15,6 @@ use sov_universal_wallet::UniversalWallet;
 // Hack - because the macro is configured to be re-exported from sov_rollup_interface;
 // but _we_ are a dependency of sov_rollup_interface so we can't import it without causing a cycle
 // This should not be an issue anywhere else except inside this crate's tests right here
-mod sov_rollup_interface {
-    pub use sov_universal_wallet;
-}
 
 // TODO: there's probably a better way than nested macros
 macro_rules! encode_decode_tests_simple {
